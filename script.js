fbcc942af5be468d2a393af832010fc4f30be5cb
siyanandan <chaos69480@gmail.com>
2023-01-20 10:58:44 +0530

    samj gya

--- script.js
+++ script.js
@@ -11,8 +11,15 @@ fetch("./test.json")
     const card = userCardTemplate.content.cloneNode(true).children[0]
     const header = card.querySelector("[data-header]")
     const body = card.querySelector("[data-body]")
-    header.textContent = user.word
-    body.textContent = user.alternative.join(", ")
+
+    alternatives = user.alternative
+
+    for(var i = 0 ; i < alternatives.length ; i++){
+        alternatives[i] = alternatives[i][0].toUpperCase() + alternatives[i].substring(1)
+    }       
+
+    header.textContent = user.word[0].toUpperCase() + user.word.substring(1)
+    body.textContent = alternatives.join(", ")
     userCardContainer.append(card)
 })
 });
