7fab5fe159c00e3c5163b9bd756ab4ba99b13bad
siyanandan <chaos69480@gmail.com>
2023-01-20 12:34:17 +0530

    samj gya

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 const userCardTemplate = document.querySelector("[data-user-template")
 const userCardContainer = document.querySelector("[data-user-card-container")
+const searchInput = document.querySelector("[data-search]")
 
+let users = []
+
+searchInput.addEventListener("input", (e) => {
+    const value = e.target.value.toLowerCase()
+    
+    words.forEach(word => {
+        const isVisible = word.word.toLowerCase().includes(value) 
+        word.element.classList.toggle("hide", !isVisible)
+    })
+
+})
 
 fetch("./test.json")
 .then(response => {
    return response.json();
 })
 .then(data => {
-    data.forEach(user => {
+    words = data.map(word => {
     const card = userCardTemplate.content.cloneNode(true).children[0]
     const header = card.querySelector("[data-header]")
     const body = card.querySelector("[data-body]")
 
-    alternatives = user.alternative
+    alternatives = word.alternative
 
     for(var i = 0 ; i < alternatives.length ; i++){
         alternatives[i] = alternatives[i][0].toUpperCase() + alternatives[i].substring(1)
     }       
 
-    header.textContent = user.word[0].toUpperCase() + user.word.substring(1)
+    header.textContent = word.word[0].toUpperCase() + word.word.substring(1)
     body.textContent = alternatives.join(", ")
     userCardContainer.append(card)
+    return { word: word.word[0].toUpperCase() + word.word.substring(1), alternative: alternatives.join(", "), element: card}
 })
 });
